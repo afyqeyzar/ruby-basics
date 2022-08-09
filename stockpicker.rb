8586cebb1561ca3d30bbc54e956fb7d4d6e10f48
afyqeyzar <norafyqe@gmail.com>
2022-08-09 17:32:43 +0100

    added stockpicker.rb

---
+++ stockpicker.rb
@@ -0,0 +1,24 @@
+def stock_picker(prices)
+    min_price = prices[0]
+    min_index = 0
+
+    profit = 0
+    days = [0,0]
+
+    prices.each_with_index do |price, index|
+        if price < min_price
+            min_price = price
+            min_index = index
+            
+        end
+
+        if price - min_price > profit
+            profit = price - min_price
+            days = [min_index,index]
+        end
+    end
+    days
+
+end    
+
+p stock_picker([18,19,17,3,6,9,15,8,6,1,10])
